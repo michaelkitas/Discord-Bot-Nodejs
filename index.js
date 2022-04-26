16ea3e93c4d73599a2a406060b2452c9a0a9fe80
michaelkitas <mixaliskitas@gmail.com>
2022-04-26 21:20:04 +0300

    How to Create a Discord Bot

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, Intents } = require("discord.js");
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "Hello") {
+    msg.channel.send("Hello!");
+  }
+});
+
+client.login("YOUR_BOT_TOKEN");
